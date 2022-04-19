fe3ac33b11f9a1a9465bef55c85f0d1fa667bd73
Cody P Schafer <dev@codyps.com>
2022-04-18 23:52:31 -0400

    more bench

--- benches/compare.rs
+++ benches/compare.rs
@@ -1,16 +1,16 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rand::RngCore;
 
-pub fn crc_xmodem(c: &mut Criterion) {
+fn crc_xmodem(c: &mut Criterion) {
     let mut group = c.benchmark_group("xmodem");
     let mut rng = rand::thread_rng();
     let mut buf = [0u8; 4096];
     rng.fill_bytes(&mut buf);
-    group.bench_function("crc-ccitt XMODEM", |b| {
+    group.bench_function("crc-ccitt", |b| {
         b.iter(|| crc_ccitt::CRC_16_XMODEM.checksum(black_box(&buf)))
     });
 
-    group.bench_function("crc-any xmodem", |b| {
+    group.bench_function("crc-any", |b| {
         b.iter(|| {
             let mut crc_any = crc_any::CRCu16::crc16xmodem();
             crc_any.digest(black_box(&buf));
@@ -19,7 +19,7 @@ pub fn crc_xmodem(c: &mut Criterion) {
     });
 
     let crc = crc::Crc::<u16>::new(&crc::CRC_16_XMODEM);
-    group.bench_function("crc xmodem", |b| {
+    group.bench_function("crc", |b| {
         b.iter(|| {
             let mut digest = crc.digest();
             digest.update(black_box(&buf));
@@ -28,5 +28,32 @@ pub fn crc_xmodem(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, crc_xmodem);
+fn crc_kermit(c: &mut Criterion) {
+    let mut group = c.benchmark_group("kermit");
+    let mut rng = rand::thread_rng();
+    let mut buf = [0u8; 4096];
+    rng.fill_bytes(&mut buf);
+    group.bench_function("crc-ccitt", |b| {
+        b.iter(|| crc_ccitt::CRC_16_KERMIT.checksum(black_box(&buf)))
+    });
+
+    group.bench_function("crc-any", |b| {
+        b.iter(|| {
+            let mut crc_any = crc_any::CRCu16::crc16kermit();
+            crc_any.digest(black_box(&buf));
+            black_box(crc_any.get_crc())
+        })
+    });
+
+    let crc = crc::Crc::<u16>::new(&crc::CRC_16_KERMIT);
+    group.bench_function("crc", |b| {
+        b.iter(|| {
+            let mut digest = crc.digest();
+            digest.update(black_box(&buf));
+            black_box(digest.finalize());
+        })
+    });
+}
+
+criterion_group!(benches, crc_kermit, crc_xmodem);
 criterion_main!(benches);
